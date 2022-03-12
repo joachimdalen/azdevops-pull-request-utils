2f95c62a10f653125f573be66f4e0f0fdb04e146
Joachim Dalen <15696189+joachimdalen@users.noreply.github.com>
2022-03-12 06:18:51 +0100

    Fixes (#3)

--- tasks/pull-request-status/webpack.config.js
+++ tasks/pull-request-status/webpack.config.js
@@ -1,22 +1,28 @@
-var path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+var path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./dist/index.js",
-  target: "node",
+  mode: 'development',
+  entry: './dist/index.js',
+  target: 'node',
   output: {
-    path: path.resolve(__dirname, "dist/bundle"),
-    library: "pull-request-status",
-    libraryTarget: "umd",
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist/bundle'),
+    library: 'pull-request-status',
+    libraryTarget: 'umd',
+    filename: 'index.js'
+  },
+  resolve: {
+    alias: {
+      'azure-devops-extension-api': path.resolve('node_modules/azure-devops-extension-api'),
+      'azure-pipelines-task-lib': path.resolve('node_modules/azure-pipelines-task-lib')
+    }
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "./icon.png", to: "icon.png" },
-        { from: "./task.json", to: "task.json" },
-      ],
-    }),
-  ],
+        { from: './icon.png', to: 'icon.png' },
+        { from: './task.json', to: 'task.json' }
+      ]
+    })
+  ]
 };
